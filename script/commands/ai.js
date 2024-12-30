5ddb3931b1e3f83a09629e4de3dc88c38876583f
Ryuko <139643556+ryukodeveloper@users.noreply.github.com>
2024-12-31 00:54:02 +0800

    update script/commands/ai.js

---
+++ script/commands/ai.js
@@ -0,0 +1,30 @@
+module.exports.config = {
+  name: `ai`,
+  version: "1.1.0",
+  permission: 0,
+  credits: "ryuko",
+  description: "",
+  prefix: false,
+  premium: true,
+  category: "without prefix",
+  usage: `ai (question)`,
+  cooldowns: 3,
+  dependency: {
+    "hercai": ""
+  }
+};
+
+module.exports.run = async function({api, event, args}) {
+  const message = args.join(" ");
+  if (!message) {
+    return api.sendMessage('please provide a question');
+  }
+const { Hercai } = require('hercai');
+const herc = new Hercai();
+herc.question({
+  model: "v3",
+  content: message
+}).then(response => {
+api.sendMessage(response.reply, event.threadID, event.messageID);
+});
+}
