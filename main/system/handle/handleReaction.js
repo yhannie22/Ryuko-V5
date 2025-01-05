93703467615b6014522e2dbc86ef3f96f398ce40
Ryuko <139643556+ryukodeveloper@users.noreply.github.com>
2025-01-05 20:59:42 +0800

    update main/system/handle/handleReaction.js

--- main/system/handle/handleReaction.js
+++ main/system/handle/handleReaction.js
@@ -1,5 +1,5 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
-    return function ({ event }) {
+    return async function ({ event }) {
         const { handleReaction, commands } = global.client;
         const { messageID, threadID } = event;
         const bots = require("../../../bots.json");
